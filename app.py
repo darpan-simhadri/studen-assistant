cda1b39187b19d749462831ab18183bfc4be2af3
darpan-simhadri <darpansimhadri@gmail.com>
2026-08-04 14:22:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-import os
-import gradio as gr
+import os 
 from google import genai
 from google.genai import types
-api_key = os.getenv("GEMINI_API")
-client =genai.Client(api_key=api_key)
+import gradio as gr
+
+client = genai.Client(api_key=os.getenv('GEMINI_API'))
 
 personalities = {
   "Friendly":
@@ -11,19 +11,26 @@ personalities = {
   "Academic":
   "You are a strictly academic, highly detailed, and professional university Professor. Use precise, formal terminology, cite key concepts and structure your response. Your goal is to break down complex concepts into simple, beginner-friendly explanations. Use analogies and real-world examples that beginners can relate to. Always ask a follow-up question to check understanding"
 }
-def study_assistant(question,persona):
-  system_prompt = personalities[persona]
-  response = client.models.generate_content(
-    model="gemini-2.5-flash",
-    config=types.GenerateContentConfig(
-        system_instruction=system_prompt,
-        temperature=0.7,
-        max_output_tokens=1000,
-    ),
-    contents=question
-  )
-  return response
-  demo= gr.Interface(
+
+def study_assistant(question, persona):
+    system_prompt = personalities[persona]
+
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        config=types.GenerateContentConfig(
+            system_instruction=system_prompt,
+            temperature=0.4,
+            max_output_tokens=1000
+        ),
+        contents=question
+    )
+    return response.text
+
+question = "What are LLMs?"
+personality = "Friendly"
+print(study_assistant(question, personality))
+
+demo = gr.Interface(
     fn=study_assistant,
     inputs=[
         gr.Textbox(lines=4, placeholder="Ask a question...", label="Question"),
@@ -33,4 +40,5 @@ def study_assistant(question,persona):
     title="Study Assistant",
     description="Ask a question and get an answer from your AI study assistant with a chosen personality."
 )
-demo.launch(server_name="0.0.0.0",server_port=int(os.environ.get("PORT",7860)))
+demo.launch(server_name="0.0.0.0",
+            server_port=int(os.environ.get("PORT",7860)))
